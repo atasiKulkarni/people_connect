a04e41fbb7b4a28732b08bae1c639da3edaa672a
Atasi Kulkarni <atasi.kulkarni@bajajfinserv.in>
2026-02-19 16:28:08 +0530

    added db connect

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,22 @@
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "people-connect",
+//   password: "bts@2025",
+//   port: 5432,
+// });
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "people-connect",
-  password: "bts@2025",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: false }
+    : false
 });
+
 module.exports = pool;
+``
