0cdab61ed3ad6bb78cf551bd87dce570df185e49
Atasi Kulkarni <atasi.kulkarni@bajajfinserv.in>
2026-03-09 15:12:41 +0530

    added baseUrl

--- UI/src/features/Banner/utility/BannerUtility.ts
+++ UI/src/features/Banner/utility/BannerUtility.ts
@@ -6,6 +6,7 @@ export enum BannerUrlType {
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export const getBannerScreenRequestURL = (urlType: BannerUrlType) => {
+  console.log("BASE_URL-->", baseUrl);
   switch (urlType) {
     case BannerUrlType.banner:
       return `${baseUrl}${API_ENDPOINTS.BANNER}`;
