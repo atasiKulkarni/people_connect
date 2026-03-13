24ebccca58da5904758e5c8d389563118b6186e0
Atasi Kulkarni <atasi.kulkarni@bajajfinserv.in>
2026-03-14 00:49:38 +0530

    cors issue resolved

--- Backend/app.js
+++ Backend/app.js
@@ -31,14 +31,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    // origin: allowedOrigins,
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
