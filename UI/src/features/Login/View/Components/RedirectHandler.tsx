98431b617e315bd66ad4bec70718368da69769c0
Atasi Kulkarni <atasi.kulkarni@bajajfinserv.in>
2026-03-18 13:52:18 +0530

    added loginRequest for sso login

--- UI/src/features/Login/View/Components/RedirectHandler.tsx
+++ UI/src/features/Login/View/Components/RedirectHandler.tsx
@@ -5,6 +5,10 @@ import { useDispatch } from "react-redux";
 import { SSOLogin } from "../../action/LoginAction";
 import { apiRequest } from "../../../../authConfig";
 import type { AppDispatch } from "../../../../redux/store";
+import {
+  InteractionRequiredAuthError,
+  type AuthenticationResult,
+} from "@azure/msal-browser";
 
 export const RedirectHandler = () => {
   const { instance, accounts } = useMsal();
@@ -12,27 +16,53 @@ export const RedirectHandler = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    console.log("RedirectHandler mounted");
+    console.log("RedirectHandler mounted - checking for redirect response");
+    
     instance.handleRedirectPromise().then((response) => {
+      console.log("handleRedirectPromise response:", response);
+      
       if (response) {
         // User was redirected back after SSO login
         const account = response.account || accounts[0];
+        console.log("Account found:", account);
         
         if (account) {
           // Acquire token and login
           acquireTokenAndLogin(account);
         }
+      } else {
+        console.log("No redirect response - user not coming from Azure AD");
       }
+    }).catch((err) => {
+      console.error("handleRedirectPromise error:", err);
     });
-  }, [instance, accounts]);
+  }, [instance, accounts, dispatch, navigate]);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const acquireTokenAndLogin = async (account: any) => {
     try {
-      const tokenRes = await instance.acquireTokenSilent({
-        account,
-        scopes: apiRequest.scopes,
-      });
+      console.log("Attempting to acquire token...");
+      
+      let tokenRes: AuthenticationResult;
+      
+      try {
+        tokenRes = await instance.acquireTokenSilent({
+          account,
+          scopes: apiRequest.scopes,
+        });
+        console.log("Token acquired silently:", tokenRes.accessToken);
+      } catch (err) {
+        if (err instanceof InteractionRequiredAuthError) {
+          console.log("Silent acquisition failed, using popup...");
+          tokenRes = await instance.acquireTokenPopup({
+            account,
+            scopes: apiRequest.scopes,
+          });
+          console.log("Token acquired via popup:", tokenRes.accessToken);
+        } else {
+          throw err;
+        }
+      }
 
       const accessToken = tokenRes.accessToken;
       if (!accessToken) {
@@ -40,12 +70,16 @@ export const RedirectHandler = () => {
         return;
       }
 
+      console.log("Dispatching SSOLogin with token...");
       dispatch(SSOLogin(accessToken))
         .unwrap()
-        .then(() => {
+        .then((response) => {
+          console.log("LOGIN_RESPONSE:", response);
           navigate("/home");
         })
-        .catch((err) => console.log("Error:", err));
+        .catch((err) => {
+          console.error("SSOLogin dispatch error:", err);
+        });
     } catch (err) {
       console.error("Token acquisition error:", err);
     }
--- UI/src/features/Login/View/Screen/Login.tsx
+++ UI/src/features/Login/View/Screen/Login.tsx
@@ -1,72 +1,84 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Image } from "../../../../utility/Image";
 import { useMsal } from "@azure/msal-react";
-import { loginRequest, apiRequest } from "../../../../authConfig";
-import {
-  InteractionRequiredAuthError,
-  type AuthenticationResult,
-} from "@azure/msal-browser";
-import { SSOLogin } from "../../action/LoginAction";
-import type { AppDispatch } from "../../../../redux/store";
-import { useDispatch } from "react-redux";
+import { loginRequest } from "../../../../authConfig";
+// import {
+//   InteractionRequiredAuthError,
+//   type AuthenticationResult,
+// } from "@azure/msal-browser";
+// import { SSOLogin } from "../../action/LoginAction";
+// import type { AppDispatch } from "../../../../redux/store";
+// import { useDispatch } from "react-redux";
 
 export const Login = () => {
-  const { instance, accounts } = useMsal();
-  const navigation = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
+  const { instance } = useMsal();
+  // const navigation = useNavigate();
+  // const dispatch = useDispatch<AppDispatch>();
   const GoToDashboard = async () => {
-  
     try {
-      console.log("instance-->", instance);
-      // 1) Sign in
+      console.log("Starting SSO login redirect...");
+      // This redirects away - nothing after this executes
       await instance.loginRedirect(loginRequest);
-      const loginRes = await instance.handleRedirectPromise();
-      console.log("loginRes->", loginRes);
-      // The account MSAL should use for token acquisition
-      const account = loginRes?.account || accounts[0];
-      if (!account) {
-        console.error("No account after login");
-        return;
-      }
-      // 2) Acquire ACCESS TOKEN for your API
-      let tokenRes: AuthenticationResult;
-      try {
-        console.log("apiRequest-->", apiRequest);
-        tokenRes = await instance.acquireTokenSilent({
-          account,
-          scopes: apiRequest.scopes,
-        });
-        console.log("tokenRes->", tokenRes);
-      } catch (err) {
-        if (err instanceof InteractionRequiredAuthError) {
-          tokenRes = await instance.acquireTokenPopup({
-            account,
-            scopes: apiRequest.scopes,
-          });
-        } else {
-          throw err;
-        }
-      }
-      const accessToken = tokenRes.accessToken;
-      if (!accessToken) {
-        console.error("No access token acquired");
-        return;
-      }
-      console.log("accessToken-->", accessToken);
-
-      dispatch(SSOLogin(accessToken))
-        .unwrap()
-        .then((response) => {
-          console.log("LOGIN_RESPONSE-->:", response);
-          navigation("/home");
-        })
-        .catch((err) => console.log("Error:", err));
     } catch (e) {
       console.error("Login Error:", e);
       alert("Login failed. See console for details.");
     }
   };
+
+  // const GoToDashboard = async () => {
+  
+  //   try {
+  //     console.log("instance-->", instance);
+  //     // 1) Sign in
+  //     const loginRes: AuthenticationResult = await instance.loginPopup(
+  //       loginRequest
+  //     );
+  //     console.log("loginRes->", loginRes);
+  //     // The account MSAL should use for token acquisition
+  //     const account = loginRes.account || accounts[0];
+  //     if (!account) {
+  //       console.error("No account after login");
+  //       return;
+  //     }
+  //     // 2) Acquire ACCESS TOKEN for your API
+  //     let tokenRes: AuthenticationResult;
+  //     try {
+  //       console.log("apiRequest-->", apiRequest);
+  //       tokenRes = await instance.acquireTokenSilent({
+  //         account,
+  //         scopes: apiRequest.scopes,
+  //       });
+  //       console.log("tokenRes->", tokenRes);
+  //     } catch (err) {
+  //       if (err instanceof InteractionRequiredAuthError) {
+  //         tokenRes = await instance.acquireTokenPopup({
+  //           account,
+  //           scopes: apiRequest.scopes,
+  //         });
+  //       } else {
+  //         throw err;
+  //       }
+  //     }
+  //     const accessToken = tokenRes.accessToken;
+  //     if (!accessToken) {
+  //       console.error("No access token acquired");
+  //       return;
+  //     }
+  //     console.log("accessToken-->", accessToken);
+
+  //     dispatch(SSOLogin(accessToken))
+  //       .unwrap()
+  //       .then((response) => {
+  //         console.log("LOGIN_RESPONSE-->:", response);
+  //         navigation("/home");
+  //       })
+  //       .catch((err) => console.log("Error:", err));
+  //   } catch (e) {
+  //     console.error("Login Error:", e);
+  //     alert("Login failed. See console for details.");
+  //   }
+  // };
   return (
     <div className="grid grid-cols-8 gap-4 w-full h-screen bg-white">
       <div className="col-span-5 w-full h-full flex flex-col justify-center items-center pt-5">
